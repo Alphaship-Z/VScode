1cfa9d73b59ea9522c68478cf4edfc817c264089
Alphaship <k19i53n7@gmail.com>
2024-02-24 20:44:42 +0530

    Importing Tailwind css aand not working on JS

--- JavaScript Course/Course-Part2/tests-jasmine/tests/cartTestJas.js
+++ JavaScript Course/Course-Part2/tests-jasmine/tests/cartTestJas.js
@@ -2,8 +2,8 @@ import {cart, addToCart, loadFromStorage} from '../../data/cart.js'
 
 describe('test-suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
+    spyOn(localStorage, 'setItem');
     spyOn(localStorage,'getItem').and.callFake(() => {
-      spyOn(localStorage, 'setItem');
 
       return JSON.stringify([{
         productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
